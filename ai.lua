f048ae55fb502836a4f628f3eb8c4f8f05d7f322
Alex Wennerberg <alexwennerberg@gmail.com>
2016-03-22 22:58:44 -0500

    refactoring, setting up testing framework

--- ai.lua
+++ ai.lua
@@ -1,20 +1,17 @@
 module("ai", package.seeall)
 
-was_here = {}
-correct_path = {}
-default_test_map = {{'X', 'X', 'X', 'X', 'X'},
-					{'X', 'NWLK', 'NWLK', 'NWLK', 'X'},
-					{'X', 'NWLK', 'WALK', 'NWLK', 'X'},
-					{'X', 'NWLK', 'NWLK', 'NWLK', 'X'},
-					{'X', 'X', 'X', 'X', 'X'}}
-
-function check_if_open(startx, starty) --funciton that checks if a given coord in a closed region
-	for key,value in pairs(default_test_map) do
+function check_if_open(map, startx, starty) --funciton that checks if a given coord in a closed region
+	was_here = {}
+	correct_path = {}
+	map_to_check = {} 
+	for key,value in pairs(map) do
 		was_here[key] = {}
 		correct_path[key] = {}
+		map_to_check[key] = {}
 		for subkey,subvalue in pairs(value) do
 			was_here[key][subkey] = false
 			correct_path[key][subkey] = false
+			map_to_check[key][subkey] = map[key][subkey] --copies input map
 		end
 	end
 	return recursive_solve(startx, starty)
@@ -22,8 +19,8 @@ end
 
 function recursive_solve(x, y) --i copied this from https://en.wikipedia.org/wiki/Maze_solving_algorithm
 	print('doing recursive solve', x, y)
-	if default_test_map[x][y] == 'X' then return true end
-	if default_test_map[x][y] == 'NWLK' or was_here[x][y] then return false end
+	if map_to_check[x][y] == 'X' then return true end
+	if map_to_check[x][y] == 'NWLK' or was_here[x][y] then return false end
 	was_here[x][y] = true
 	if recursive_solve(x-1, y) then
 		--correct_path[x][y] = true;
@@ -43,7 +40,5 @@ function recursive_solve(x, y) --i copied this from https://en.wikipedia.org/wik
 	end
 	return false
 end
-
-print(check_if_open(3,3))
 
 --learn unit testing!
--- test.lua
+++ test.lua
@@ -1,24 +1,24 @@
 --run in terminal
 require "movement"
 require "ai"
+require 'busted.runner'()
 
+closed_test_map = {{'X', 'X', 'X', 'X', 'X'},
+					{'X', 'NWLK', 'NWLK', 'NWLK', 'X'},
+					{'X', 'NWLK', 'WALK', 'NWLK', 'X'},
+					{'X', 'NWLK', 'NWLK', 'NWLK', 'X'},
+					{'X', 'X', 'X', 'X', 'X'}}
 
+open_test_map = {{'X', 'X', 'X', 'X', 'X'},
+					{'X', 'NWLK', 'NWLK', 'NWLK', 'X'},
+					{'X', 'NWLK', 'WALK', 'NWLK', 'X'},
+					{'X', 'NWLK', 'WALK', 'NWLK', 'X'},
+					{'X', 'X', 'X', 'X', 'X'}}
 
-
---[[require 'busted.runner'()
-
-
-
-
-function foo()
-	return true
-end
 
 describe("a test", function()
-	it("tests some assertions", function()
-		assert.is_true(true)
-		assert.True(foo())
-		assert.are.equal(1,1)
-		assert.has.errors(function() error("this will fail") end)
+	it("tests simple maps", function()
+		assert.is_true(ai.check_if_open(open_test_map,3,3))
+		assert.is_false(ai.check_if_open(closed_test_map,3,3))
 	end)
-end)]]
+end)
